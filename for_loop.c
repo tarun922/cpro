7dc702ca93571781bf050b871ec1fed8a2d183d0
tarun922 <tarunkum8800@gmail.com>
2026-08-09 18:09:15 +0530

    for  loop

---
+++ for_loop.c
@@ -0,0 +1,22 @@
+// for loop
+
+#include <stdio.h>
+
+int main(){
+
+    // for loop is also same as while loop but its loop for a range
+    /*
+    usage of for loop how to use
+    for(define a variable ; condition ; the increment or decrement of the valiable ){
+        your code here
+
+}
+
+     */
+    // printing numbers from 1 to 10 using for loop
+    for (int i = 1 ; i <=10 ;i++){
+        printf("%d \n " , i );
+    }
+    // thats it for for loop it's easy peezy '
+    return 0;
+}
