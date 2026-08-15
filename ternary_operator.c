764d857b4b7e66047682d1fecfb079d76ad1d062
tarun922 <tarunkum8800@gmail.com>
2026-08-15 20:22:05 +0530

    ternary_operator

---
+++ ternary_operator.c
@@ -0,0 +1,15 @@
+//ternary_operator
+
+#include <stdio.h>
+
+int main(){
+    //ternary_operator is a short hand form of if else statement
+    // here it's syntax '
+    // (condition ) ? value_if_its_true : value_if_its_false ;
+
+    int a= 10;
+
+    printf("%s \n", (a > 5) ? "greater \n" : "less \n" );
+    return 0;
+
+}
