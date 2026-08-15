cebc96839ff487eb87d80984e7cfe288ef0d24d2
tarun922 <tarunkum8800@gmail.com>
2026-08-15 17:36:11 +0530

    array 3

--- arrays.c
+++ arrays.c
@@ -31,6 +31,38 @@ int main(){
 
     }
 
+    //2d array
+    // 2d array have 2 dimentions one is row and other is column
+
+    int num2d[][3]={
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+        };
+
+    //how to access elements
+
+    // array[write row num starts with 0 ][write column  num starts with 0 ]
+
+    printf("%d \n" , num[0][0]);// output =1
+    printf("%d \n" , num[0][1]);// output =2
+    printf("%d \n" , num[0][2]);// output =3
+    printf("%d \n" , num[1][0]);// output =4
+    printf("%d \n" , num[1][1]);// output =5
+    printf("%d \n" , num[1][2]);// output =6
+    printf("%d \n" , num[0][0]);// output =7
+    printf("%d \n" , num[0][1]);// output =8
+    printf("%d \n" , num[0][2]);// output =9
+
+    //u can also update element of arrays
+    // js call the index of that element and assign new value
+    //for example
+    int y[]={1,2,3};
+    printf("%d \n" , y[1]); // output = 2
+    y[1]=500;
+    printf("%d \n" , y[1]); // output = 500
+
+
 
     return 0;
 }
