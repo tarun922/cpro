41451976237051b8b8383ddbe3f6ca72a7dae03b
tarun922 <tarunkum8800@gmail.com>
2026-08-19 20:02:10 +0530

    typedef

---
+++ typedef.c
@@ -0,0 +1,18 @@
+// typedef
+
+
+#include <stdio.h>
+
+typedef char faaah[30];
+
+int main(){
+    //typedef it is somthing that u can use to give nickname to your data types
+    // sounds fun lets try it
+    // usage do this out side any function
+    //typedef existing_data_type your_nickname;
+
+    // now u can define a string by using our nickname faaah
+    faaah name="lets goo worked ";
+    printf("%s",name);
+    return 0;
+}
